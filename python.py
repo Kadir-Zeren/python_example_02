3d9815d0e1e03862aceeb6f3812da99ed6606b0d
kadir-zeren <k4dzeren@gmail.com>
2024-12-29 13:23:40 +0300

    python practice

---
+++ python.py
@@ -0,0 +1,131 @@
+# buradaki fonksiyon kare alma fonksiyonu
+
+print('kullanici ismi') # Burada kullanici ismi yazdiriliyor
+
+x = 5 # burada x degeri atandi
+
+# bu bir comment
+# bu ikinci comment
+# bu ucuncu comment
+
+a = "bu benim yazdigim string \
+stringe devam ediyorum"
+
+print(a)
+
+a = "bu benim yazdigim string \nstringe devam ediyorum"
+
+print(a)
+
+school = 'clarusway'
+
+print(school)
+
+sayi = 35
+
+print(sayi)
+
+
+ğğğ = 5
+
+print(ğğğ)
+
+okul = 'Clarusway'
+
+print(okul)
+
+help('keywords')
+
+variable = 'degisken'
+
+isim_soyisim = 'ali guzel'
+
+degisken = 'bu bir string'
+print(degisken)
+
+degisken = 5
+print(degisken)
+
+a = 'degisken_1'
+b = 5
+c = 7.6
+print(a,b,c)
+
+january = march = may = july = august = october = december = 31
+
+april = june = september = november = 30
+
+february = 28
+
+print(january, february, march, april, may, june, july, august, september, october, november, december)
+
+ali = None
+
+print(ali)
+
+x = 15 
+y = 33 
+z = x 
+x = y 
+
+print(x)
+print(y)
+print(z)
+
+a, b, c = 5, 7, 9
+
+print(a,b,c)
+
+a = 'clarusway'
+
+print(type(a))
+
+sehir = '35'
+
+print(type(sehir))
+
+sayi1 = 40
+
+print(type(sayi1))
+
+a = 5.7
+
+print(type(a))
+
+b = True
+
+print(type(a), type(b))
+
+a = 123
+
+print(type(a))
+
+print(str(a))
+
+yeni_a = str(a)
+
+print(type(yeni_a))
+
+pi = 3.14
+
+print(pi)
+
+a = 1.23
+
+print(type(a))
+
+print(int(a))
+
+print(int(234.987))
+
+tamsayi = 7
+
+print(type(tamsayi))
+
+print(float(tamsayi))
+
+print(5 + 3)
+
+print(5 + 3.0)
+
+print('123' + '12')
